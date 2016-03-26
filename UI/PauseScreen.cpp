93d465b58c535c4c937ff6922be1127d2d0e946d
Franco Catrin <fcatrin@gmail.com>
2016-03-26 05:23:53 +0000

    Modify Pause Screen to show only the custom menu

--- UI/PauseScreen.cpp
+++ UI/PauseScreen.cpp
@@ -274,6 +274,7 @@ void GamePauseScreen::CreateViews() {
 
 	root_ = new LinearLayout(ORIENT_HORIZONTAL);
 
+	/*
 	ViewGroup *leftColumn = new ScrollView(ORIENT_VERTICAL, new LinearLayoutParams(1.0, scrollMargins));
 	root_->Add(leftColumn);
 
@@ -330,6 +331,7 @@ void GamePauseScreen::CreateViews() {
 	}
 	rightColumnItems->Add(new Spacer(25.0));
 	rightColumnItems->Add(new Choice(pa->T("Exit to menu")))->OnClick.Handle(this, &GamePauseScreen::OnExitToMenu);
+	*/
 }
 
 UI::EventReturn GamePauseScreen::OnGameSettings(UI::EventParams &e) {
--- git-version.cpp
+++ git-version.cpp
@@ -1,6 +1,3 @@
 // This is a generated file.
 
-const char *PPSSPP_GIT_VERSION = "v1.2.1-63-gb137bf1";
-
-// If you don't want this file to update/recompile, change to 1.
-#define PPSSPP_GIT_VERSION_NO_UPDATE 0
+const char *PPSSPP_GIT_VERSION = "unknown";
